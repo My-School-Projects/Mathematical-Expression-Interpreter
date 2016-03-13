887b81bd91423ba3f443f7f60f992fadc84e00ef
Michael Dorst <code@mikefdorst.com>
2016-03-12 16:52:31 -0800

    Get rid of App

--- src/com/mdorst/Main.java
+++ src/com/mdorst/Main.java
@@ -1,9 +1,14 @@
 package com.mdorst;
 
+import com.mdorst.util.TestRunner;
+
 public class Main {
 
     public static void main(String[] args) {
-        App app = new App();
-        app.main();
+        TestRunner test = new TestRunner();
+        Interpreter interpreter = new Interpreter();
+        interpreter.interpret("1 + 2");
+        test.assertEqual(interpreter.result(), 3, "result");
+        test.done();
     }
 }
