69732d2c8bbf2cf921cc4a7514c82e2ab5f5dbcd
Michael Dorst <code@mikefdorst.com>
2016-03-17 04:04:41 -0700

    Reimplement parser

--- src/com/mdorst/Interpreter.java
+++ src/com/mdorst/Interpreter.java
@@ -8,190 +8,160 @@ import com.mdorst.exception.SyntaxError;
 import com.mdorst.exception.UnrecognizedSymbolError;
 
 import java.util.StringTokenizer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Interpreter {
 
-    private class Token {
-        private double value;
-        private String name;
-        public Token(double v) {
-            value = v;
-        }
-        public Token(String n) {
-            name = n;
-        }
-        public double value() {
-            if (name == null) {
-                return value;
-            } else {
-                return symbols.get(name);
-            }
-        }
-        public String name() {
-            return name;
-        }
-        public Token call(Token op1, Token op2) {
-            return operators.get(name).call(op1, op2);
-        }
-        public boolean isOperator() {
-            return name != null && operators.get(name) != null;
-        }
-        @Override
-        public String toString() {
-            if (name == null) {
-                return Double.toString(value);
-            } else {
-                return name;
-            }
-        }
-    }
-
-    /**
-     * Standard interface for all operations (+, -, *, etc.)
-     */
-    private interface Operation {
-        Token call(Token op1, Token op2);
-    }
-
-    private class Operator {
-        private int priority;
-        private Operation operation;
-
-        public Operator(int p, Operation op) {
-            priority = p;
-            operation = op;
-        }
-        public int priority() {
-            return priority;
-        }
-        public Token call(Token op1, Token op2) {
-            return operation.call(op1, op2);
-        }
-    }
-
-    /**
-     * Maps string representations of operators ("+", "-", "*", etc.)
-     * to functions which can perform them.
-     * Usage:
-     * operators.get("+").call(3, 2) // result: 5
-     */
-    private static HashMap<String, Operator> operators;
     /**
      * Maps variable names to values.
      */
     private HashMap<String, Double> symbols = new HashMap<>(String::hashCode);
 
-    public Interpreter() {
-        /**
-         * Set up {@code operators} to be accessed via string identifiers.
-         * Do this only once, upon the first call to this constructor.
-         */
-        if (operators == null) {
-            operators = new HashMap<>(String::hashCode);
-            operators.add("+", new Operator(3, (op1, op2) -> new Token(op1.value + op2.value)));
-            operators.add("-", new Operator(3, (op1, op2) -> new Token(op1.value - op2.value)));
-            operators.add("*", new Operator(2, (op1, op2) -> new Token(op1.value * op2.value)));
-            operators.add("/", new Operator(2, (op1, op2) -> new Token(op1.value / op2.value)));
-            operators.add("=", new Operator(0, (Token op1, Token op2) -> {
-                symbols.add(op1.name, op2.value());
-                return new Token(op2.value());
-            }));
-        }
-    }
-
     /**
-     * Takes an expression, stores it, then interprets it, and returns the result.
+     * Takes an expression, interprets it, and returns the result.
      * @param expression The expression to be interpreted
      * @return The result of the expression
      */
     public double interpret(String expression) throws SyntaxError {
-        LinkedList<Token> expr = new LinkedList<>();
+        LinkedList<String> expr = new LinkedList<>();
         /**
          * Tokenize the expression
          */
         {
             StringTokenizer tokenizer = new StringTokenizer(expression, "+-*/= \n\t\r\f", true);
             while (tokenizer.hasMoreTokens()) {
-                expr.add(new Token(tokenizer.nextToken()));
+                expr.add(tokenizer.nextToken());
             }
         }
         /**
          * Create postfix expression
          */
-        Stack<Token> opStack = new Stack<>();
+        Stack<String> opStack = new Stack<>();
         Queue<Token> postfix = new Queue<>();
-        for (Token token : expr) {
+        for (String token : expr) {
             /**
-             * Variables
+             * Variables:
+             * Always get pushed to the postfix expression
              */
-            if (token.name.matches("([a-z]|[A-Z]|_)\\w*")) {
-                postfix.enqueue(token);
+            if (token.matches("([a-z]|[A-Z]|_)\\w*")) {
+                postfix.enqueue(new Token(token));
             } else
             /**
-             * Numeric literals
+             * Numeric literals:
+             * Always get pushed to the postfix expression
              */
-            if (token.name.matches("\\d+")) {
-                postfix.enqueue(new Token(Double.valueOf(token.name)));
-            } else
-            /**
-             * Operators
-             */
-            if (token.name.matches("[\\+\\-*/=]")) {
-                while (!opStack.isEmpty() &&
-                        operators.get(opStack.top().name).priority < operators.get(token.name).priority) {
-                    postfix.enqueue(opStack.pop());
+            if (token.matches("\\d+")) {
+                postfix.enqueue(new Token(Double.valueOf(token)));
+            }
+            else if (token.equals("=")) {
+                if (opStack.isEmpty()) {
+                    opStack.push("=");
+                } else {
+                    String e = expression.substring(0, expression.indexOf("="));
+                    throw new SyntaxError("Cannot assign to the expression " + e);
+                }
+            }
+            else if (token.matches("\\+|-")) {
+                while (!opStack.isEmpty()) {
+                    if (opStack.top().matches("\\*|/"))
+                        postfix.enqueue(new Token(opStack.pop()));
+                    else break;
                 }
                 opStack.push(token);
-            } else
+            }
+            else if (token.matches("\\*|/")) {
+                while (!opStack.isEmpty()) {
+                    if (opStack.top().matches("\\*|/"))
+                        postfix.enqueue(new Token(opStack.pop()));
+                    else break;
+                }
+                opStack.push(token);
+            }
+            else if (token.equals("(")) {
+                opStack.push(token);
+            }
+            else if (token.equals(")")) {
+                if (opStack.isEmpty() || opStack.top().equals("=")) {
+                    throw new SyntaxError("Unmatched ')'");
+                }
+                /**
+                 * Unstack until "(" is found
+                 */
+                while (!opStack.isEmpty() && opStack.top() != "(") {
+                    postfix.enqueue(new Token(opStack.pop()));
+                }
+                if (!opStack.pop().equals("(")) {
+                    throw new SyntaxError("Unmatched ')'");
+                }
+            }
             /**
              * Anything else, excluding whitespace
              */
-            if (!token.name.matches("[ \t\n\r\f]")) {
+            else if (!token.matches("[ \t\n\r\f]")) {
                 /**
                  * Unrecognized symbol (did not match any recognized pattern)
                  * An exception will be thrown.
                  */
-                throw new UnrecognizedSymbolError(token.name + " is not a valid token");
+                throw new UnrecognizedSymbolError(token + " is not a valid token");
             }
         }
+        /**
+         * If there are any operators left on the operator stack,
+         * enqueue them onto postfix.
+         */
         while (!opStack.isEmpty()) {
-            postfix.enqueue(opStack.pop());
+            postfix.enqueue(new Token(opStack.pop()));
         }
         /**
          * Evaluate postfix expression
          */
         Stack<Token> eval = new Stack<>();
-        while (!postfix.isEmpty()) {
-            /**
-             * While there are operands in the postfix queue,
-             * push them to the eval stack
-             */
-            while (!postfix.isEmpty() && !postfix.front().isOperator()) {
-                eval.push(postfix.dequeue());
-            }
-            /**
-             * If the postfix queue is not empty and there
-             * are less than two operands in the eval stack
-             * at this point, then the expression is not valid.
-             * An exception will be thrown.
-             */
-            if (!postfix.isEmpty() && eval.size() < 2) {
-                throw new SyntaxError(expression + " is not a valid expression");
-            }
-            /**
-             * While there is more than one operand on the eval stack,
-             * pop the top two and operate on them using the next
-             * operator in the postfix queue.
-             */
-            while (eval.size() > 1) {
-                Token rightOperand = eval.pop();
-                Token leftOperand = eval.pop();
-                Token operator = postfix.dequeue();
-                eval.push(operator.call(leftOperand, rightOperand));
+        return 0;
+    }
+
+    private boolean isOperator(String token) {
+        return token.matches("[\\+\\*-/=]");
+    }
+
+    private class Token {
+        private double _value;
+        private String _name;
+
+        public Token(String name) {
+            _name = name;
+        }
+        public Token(double value) {
+            _value = value;
+        }
+
+        public String name() {
+            return _name;
+        }
+        public double value() {
+            if (_name == null) {
+                return _value;
+            } else {
+                return symbols.get(_name);
             }
         }
-        return eval.pop().value();
+    }
+
+    Operator getOperator(String token) {
+        switch (token) {
+            case "+":
+                return (op1, op2) -> op1 + op2;
+            case "-":
+                return (op1, op2) -> op1 - op2;
+            case "*":
+                return (op1, op2) -> op1 * op2;
+            case "/":
+                return (op1, op2) -> op1 = op2;
+            case "=":
+                return (op1, op2) -> 0; //TODO
+        }
+        return null;
+    }
+
+    private interface Operator {
+        public double call(double op1, double op2);
     }
 }
