547d653ca0a0b20372c692dd1a17cdab8b2193f6
Michael Dorst <code@mikefdorst.com>
2016-04-07 00:03:06 -0700

    Remove comment

--- src/com/mdorst/exception/UnrecognizedSymbolError.java
+++ src/com/mdorst/exception/UnrecognizedSymbolError.java
@@ -1,8 +1,5 @@
 package com.mdorst.exception;
 
-/**
- * Michael Dorst
- */
 public class UnrecognizedSymbolError extends SyntaxError {
     public UnrecognizedSymbolError(String message) {
         super(message);
